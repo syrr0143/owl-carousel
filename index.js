4276cd759655a71784c084ff59cee0db50b79915
syrr0143 <sy816120@gmail.com>
2024-05-23 22:38:05 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+$(document).ready(function () {
+    $('.room-slider').owlCarousel({
+        loop: true,
+        margin: 30,
+        // autoplay: true,
+        // autoplayTimeout: 3000,
+        smartSpeed: 450,
+        nav: true,
+        navText: ['<span class="custom-prev">&lt;</span>', '<span class="custom-next">&gt;</span>'], // Custom navigation icons
+        responsive: {
+            0: {
+                items: 1
+            },
+            768: {
+                items: 2
+            },
+            991: {
+                items: 3
+            },
+            1200: {
+                items: 3
+            },
+            1920: {
+                items: 3
+            }
+        }
+    });
+
+    $('.custom-next').click(function () {
+        $('.room-slider').trigger('next.owl.carousel');
+    });
+
+    $('.custom-prev').click(function () {
+        $('.room-slider').trigger('prev.owl.carousel');
+    });
+
+
+});
+
+const scrollbar = document.getElementById('swiper-scrollbar-1716482138826');
+const scrollProgress = scrollbar.querySelector('.scroll-progress');
+
+function updateScrollProgress() {
+    const scrollTop = document.documentElement.scrollTop;
+    const scrollHeight = document.documentElement.scrollHeight;
+    const clientHeight = document.documentElement.clientHeight;
+
+    const progress = scrollTop / (scrollHeight - clientHeight);
+    scrollProgress.style.width = `${progress * 100}%`;
+}
+
+scrollbar.addEventListener('scroll', updateScrollProgress);
+window.addEventListener('scroll', updateScrollProgress);
+
+updateScrollProgress(); // Initial update
